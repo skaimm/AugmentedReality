33e6311c7f7fdc226bfa3523bdb6296db2a989bc
skaimm <ua.smt@hotmail.com>
2019-09-06 18:59:40 +0300

    Bugatti Veyron AR
    Change color of the Bugatti which match on the app

---
+++ bugattiveyron/Assets/ColorPicker/ObjectColor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectColor : MonoBehaviour {
+
+	void OnSetColor(Color color)
+	{
+        
+        foreach (Renderer r in GetComponentsInChildren<Renderer>())
+        {
+            r.material.shader = Shader.Find("Transparent/Diffuse");
+            r.material.color = color;
+        }
+    }
+
+	void OnGetColor(ColorPicker picker)
+	{
+		picker.NotifyColor(GetComponent<Renderer>().material.color);
+	}
+
+}
