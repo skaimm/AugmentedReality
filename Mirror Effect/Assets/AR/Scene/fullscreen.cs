fde2aa4626bddaa01bc5fb04b6bbf7b2ac6758f5
skaimm <46901031+skaimm@users.noreply.github.com>
2020-01-08 11:26:41 +0100

    Mirror Effect on AR
    Its a graduation project to make a mirror effect on the image target. The model ,which is just for demonstrate, can change with new model.

---
+++ Mirror Effect/Assets/AR/Scene/fullscreen.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class fullscreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start() =>
+        // Toggle fullscreen
+        Screen.fullScreen = !Screen.fullScreen;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
